6b68e204195f436a1f4b7e91d07c7013660f38f2
David Sissoko <dsissoko@users.noreply.github.com>
2024-10-22 15:57:01 +0200

    code: misc

--- code/java/hmmtest1/app/src/main/java/com/r3edge/engine/examples/hmm/App.java
+++ code/java/hmmtest1/app/src/main/java/com/r3edge/engine/examples/hmm/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public String getGreeting() {
         // Comment inutile
-        log.info("ça parle bien !!!");
+        log.warn("ça parle bien !!!");
         return "Hello World!";
     }
 
